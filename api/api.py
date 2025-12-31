a24b72d917d19ab1dde3e7dd4620fa2196323c58
Iiya Zakharov <2146376@gmail.com>
2026-01-01 01:46:53 +0300

    Final commit some

--- api/api.py
+++ api/api.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from api.redis_client import redis_client
 from api.routers import router
-from api.settings import settings
 
 
 api = FastAPI()
@@ -13,15 +11,5 @@ api.add_middleware(
     allow_origins=["*"],
     allow_headers=["*"]
 )
-
-@api.on_event("startup")
-async def startup():
-    redis_client.from_url(settings.REDIS, decode_responses=True)
-
-
-@api.on_event("shutdown")
-async def shutdown():
-    if redis_client:
-        await redis_client.close()
 
 api.include_router(router, prefix="/add")
