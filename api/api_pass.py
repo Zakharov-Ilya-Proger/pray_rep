7f0cf74330232921ec5ec123f314da5d581a41f2
Iiya Zakharov <2146376@gmail.com>
2026-01-03 23:49:09 +0300

    Fix env deploy

--- api/api_pass.py
+++ api/api_pass.py
@@ -4,4 +4,4 @@ from bcrypt import checkpw
 
 async def check_password(password, api_password):
     print(password, api_password)
-    return checkpw(password.encode(), api_password)
+    return checkpw(password.encode(), api_password.encode())
