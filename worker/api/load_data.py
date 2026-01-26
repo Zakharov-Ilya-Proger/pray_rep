0369929359cb01b60a80ed7a713dcadf00543ba5
Iiya Zakharov <2146376@gmail.com>
2026-01-26 19:57:53 +0300

    Fix env deploy

--- worker/api/load_data.py
+++ worker/api/load_data.py
@@ -7,9 +7,19 @@ from worker import settings
 
 
 def download_mp3(record_id: str) -> str:
-    url = settings.API_RECORD_URL + f"?roomID={record_id}"
+    url = settings.API_RECORD_URL
 
-    resp = requests.post(url, json={"pass": settings.API_RECORD_PASS}, stream=True, timeout=(5, 120))
+    resp = requests.post(
+        url,
+        json={
+            "pass": settings.API_RECORD_PASS,
+            "roomID": int(record_id)
+        },
+        stream=True,
+        timeout=(
+            5, 120
+        )
+    )
     resp.raise_for_status()
 
     fd, path = tempfile.mkstemp(suffix=".webm")
@@ -20,6 +30,7 @@ def download_mp3(record_id: str) -> str:
             if chunk:
                 f.write(chunk)
     return path
+
 
 if __name__ == '__main__':
     print(download_mp3("92740543"))
