6da57d4fff65aac3e39b0dfce02b4722fbdd6d8d
Iiya Zakharov <2146376@gmail.com>
2026-03-11 18:49:55 +0300

    Fix env deploy

--- worker/jobs.py
+++ worker/jobs.py
@@ -1,4 +1,4 @@
 from worker.tasks import process
 
-def process_request(req_id: str, hash: str):
-    process(req_id, hash)
+def process_request(data: dict):
+    process(data['req_id'], data['hash'])
